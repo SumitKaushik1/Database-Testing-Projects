67ff5a82d31de21e69fa38e03eda578c9207bfb4
sumit <pritam.2581996@gmail.com>
2023-10-10 10:20:35 +0530

    new

---
+++ triggers_testing/test cases of trigger1.sql
@@ -0,0 +1,12 @@
+# test case 1
+insert into workCenters(`name`,capacity) values('Mold Machine',100);
+#Test query 
+select * from workCenters;
+
+-- ----------------------------------------------------------------
+
+# test case 2
+insert into workCenters(`name`,capacity) values('Packing',100);
+
+#Test Query 2
+select * from workCenters;
